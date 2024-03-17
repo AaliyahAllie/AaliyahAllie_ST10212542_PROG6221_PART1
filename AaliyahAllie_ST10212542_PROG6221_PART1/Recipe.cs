6b6e614b129bdb480251f5b68add84d8e823ee13
Aaliyah Allie <st10212542@RCCONNECT.EDU.ZA>
2024-03-17 20:51:20 +0200

    Variable declarations created

--- AaliyahAllie_ST10212542_PROG6221_PART1/Recipe.cs
+++ AaliyahAllie_ST10212542_PROG6221_PART1/Recipe.cs
@@ -8,6 +8,11 @@ namespace AaliyahAllie_ST10212542_PROG6221_PART1
 {
     public class Recipe
     {
+        public string recipeName { get; set; }
+        public List<string> ingredientNames { get; set; }
+        public List<int> ingredientQuantities { get; set; }
+        public List<string> UnitOfMeasurements { get; set; }
+        public List<String> Steps { get; set; }
 
     }
 }
